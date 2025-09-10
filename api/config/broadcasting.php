7438d64f439373309c11c5d5ed7d7893c4ebbc84
Chee3se <emilspete999@gmail.com>
2025-09-10 15:11:15 +0300

    fix ws

--- api/config/broadcasting.php
+++ api/config/broadcasting.php
@@ -4,14 +4,15 @@ return [
     'default' => env('BROADCAST_CONNECTION', 'log'),
 
     'connections' => [
-        'pusher' => [
-            'driver' => 'pusher',
-            'key' => env('PUSHER_APP_KEY'),
-            'secret' => env('PUSHER_APP_SECRET'),
-            'app_id' => env('PUSHER_APP_ID'),
+        'reverb' => [
+            'driver' => 'reverb',
+            'key' => env('REVERB_APP_KEY'),
+            'secret' => env('REVERB_APP_SECRET'),
+            'app_id' => env('REVERB_APP_ID'),
             'options' => [
-                'cluster' => env('PUSHER_APP_CLUSTER', 'eu'),
-                'useTLS' => true,
+                'host' => env('REVERB_HOST', '127.0.0.1'),
+                'port' => env('REVERB_PORT', 8080),
+                'scheme' => env('REVERB_SCHEME', 'http'),
             ],
         ],
 
